ebf5e1107f5c544912b9d7b3c00bb4a8c0f52496
vitalyj-vasilevich <vitalyj-vasilevich@gmail.com>
2019-04-04 22:40:20 +0300

    Added t3.57 task

---
+++ algot3.57/algot3.57/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace algot3._57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c, m, el, pos, i;
+            int[] table;
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                String[] input = Regex.Split(sr.ReadLine(), @"[ \r\n]+");
+                m = Convert.ToInt32(input[0]);
+                c = Convert.ToInt32(input[1]);
+                n = Convert.ToInt32(input[2]);
+                table = new int[m];
+                for (int j = 0; j < m; j++)
+                {
+                    table[j] = -1;
+                }
+                for (int j = 0; j < n; ++j)
+                {
+                    el = Convert.ToInt32(sr.ReadLine());
+                    i = 0;
+                    pos = ((el % m) + c * i) % m;
+                    while (table[pos] != -1 && table[pos] != el)
+                    {
+                        ++i;
+                        pos = ((el % m) + c * i) % m;
+                    }
+                    table[pos] = el;
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    sw.Write(table[j] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ algot3.57/algot3.57/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("algot3.57")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("algot3.57")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("78363d6e-d688-47a5-b2da-ded1e9df76ef")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
